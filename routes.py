7eb9cda88cb5eec8b38944ac6b3caff3e355cca7
minaarafat19 <aminayoussoufarafat@gmail.com>
2022-04-08 10:33:12 +0200

    test3

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
+
 from flask import Flask,render_template,url_for,request,session,redirect
 from error import eror, bet_eror
 from module_tirage import premier_tirage, deuxieme_tirage,machine,choix_cartes
+from module_gain import gain
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -16,7 +18,8 @@ def check_age():
         session['eror-form'] = eror
         return render_template('index.html')
     else:
-        session['wallet'] = request.form['wallet']
+        session['wallet'] = int(request.form['wallet'])
+        print(session['wallet'])
         return redirect(url_for('board'))
 
 @app.route('/board')
@@ -33,7 +36,7 @@ def tirage1():
     'J-c','Q-c','K-c','A-c','2-s','3-s','4-s','5-s','6-s',
     '7-s','8-s','9-s','10-s','J-s','Q-s','K-s','A-s']
     
-    session['bet'] = request.form['bet']
+    session['bet'] =int( request.form['bet'])
     if session['bet'] > session['wallet']:
         session['eror-form'] = bet_eror
         return render_template('board.html')
@@ -45,26 +48,37 @@ def tirage1():
 
         return redirect(url_for('game'))
 
-@app.route('/board/game',methods=['POST','GET'])
+@app.route('/board/game')
 def game():
     
     return render_template('game.html')
 
 
-@app.route('/board/game', methods=['POST','GET'])
+@app.route('/board/game', methods=['POST'])
 def check_card():
     
     if request.method == 'POST':
         session['tet']=request.form.getlist('i')
         print(session['tet'])
-        return str(session['tet'])
-    
-    return render_template('game2.html')
+        tirage_final= deuxieme_tirage(session['tet'],session['deck1'])
+        session['g'],res= gain(tirage_final,int(session['bet']))
+        session['wallet'] = session['wallet'] + int( session['g'])
+        session['message'] = res
+
+        if int(session['wallet']) == 0:
+            session['message'] = "you lost"
+            return redirect(url_for('board')) 
+
+       
+        
+        
+    return render_template('game2.html',tr2=tirage_final)
     
         
 
 @app.route('/board/game/game2', methods=['POST','GET'])
 def card2():
+    
     return render_template('game2.html')
 
 
